d3f71673fe55bc177a1e366940bcb79fdd36dae5
yoonsunmi <yoonsm45@naver.com>
2026-04-15 14:57:40 +0900

    init: 개발 환경 세팅

---
+++ src/main/java/com/springboot/voltstream/VoltstreamApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.voltstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoltstreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/voltstream/VoltstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.voltstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoltstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
